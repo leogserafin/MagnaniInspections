18ec69a36402b4fa229efac8a519657ca658b4ad
Gohr <joao.gohr@edu.univali.br>
2018-11-24 15:22:26 -0300

    B

--- Inspections/Inspections/Form1.Designer.cs
+++ Inspections/Inspections/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.listTabs = new System.Windows.Forms.TabControl();
             this.polesTab = new System.Windows.Forms.TabPage();
@@ -137,15 +137,15 @@
             this.poleGridView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.poleGridView.Location = new System.Drawing.Point(3, 3);
             this.poleGridView.Name = "poleGridView";
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.NullValue = "oie";
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.poleGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.NullValue = "oie";
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.poleGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.poleGridView.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.poleGridView.Size = new System.Drawing.Size(414, 267);
             this.poleGridView.TabIndex = 0;
@@ -316,6 +316,7 @@
             this.inspectionPoleIdTextBox.Location = new System.Drawing.Point(59, 3);
             this.inspectionPoleIdTextBox.Mask = "999999999999999999999999999999999";
             this.inspectionPoleIdTextBox.Name = "inspectionPoleIdTextBox";
+            this.inspectionPoleIdTextBox.PromptChar = ' ';
             this.inspectionPoleIdTextBox.Size = new System.Drawing.Size(126, 20);
             this.inspectionPoleIdTextBox.TabIndex = 9;
             this.inspectionPoleIdTextBox.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
@@ -430,7 +431,7 @@
             // 
             this.boxIdTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
             this.boxIdTextBox.Location = new System.Drawing.Point(59, 3);
-            this.boxIdTextBox.Mask = "99999999999999999999";
+            this.boxIdTextBox.Mask = "999999999999999999999999999999999";
             this.boxIdTextBox.Name = "boxIdTextBox";
             this.boxIdTextBox.PromptChar = ' ';
             this.boxIdTextBox.Size = new System.Drawing.Size(126, 20);
@@ -568,7 +569,7 @@
             // 
             this.poleIdTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
             this.poleIdTextBox.Location = new System.Drawing.Point(59, 3);
-            this.poleIdTextBox.Mask = "99999999999999999999";
+            this.poleIdTextBox.Mask = "999999999999999999999999999999999";
             this.poleIdTextBox.Name = "poleIdTextBox";
             this.poleIdTextBox.PromptChar = ' ';
             this.poleIdTextBox.Size = new System.Drawing.Size(126, 20);
